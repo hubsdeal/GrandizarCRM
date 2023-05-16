8ed7612fe83c6b478ea190d4e00f1c00853b1e03
sraban75 <sraban75@outlook.com>
2023-05-16 22:32:44 +0600

    Hub related some API updated

---
+++ aspnet-core/src/SoftGrid.Application.Shared/PublicCommon/Dtos/HubPublicViewForDropdownDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftGrid.PublicCommon.Dtos
+{
+    public class HubPublicViewForDropdownDto
+    {
+        public long Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Picture { get; set; }
+        public Guid PictureId { get; set; }
+
+        public string Url { get; set; }
+    }
+
+    public class GetAllHubsForDropdownViewBySp
+    {
+        public List<HubPublicViewForDropdownDto> Hubs { get; set; }
+
+        public GetAllHubsForDropdownViewBySp()
+        {
+            Hubs = new List<HubPublicViewForDropdownDto>();
+        }
+    }
+}
