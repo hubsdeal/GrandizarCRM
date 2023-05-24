6903a2802346553160e1bfca0a82754c42f04e72
sraban75 <sraban75@outlook.com>
2023-05-24 17:35:20 +0600

    StoreReservationSettingDto Added

---
+++ aspnet-core/src/SoftGrid.Application.Shared/Shop/Dtos/StoreReservationSettingDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+
+namespace SoftGrid.Shop.Dtos
+{
+    public class StoreReservationSettingDto : EntityDto<long>
+    {
+        public bool OfferReservation { get; set; }
+
+        public bool InstantConfirmation { get; set; }
+
+        public bool MessageStoreTeam { get; set; }
+
+        public int? MinNumberOfGuests { get; set; }
+
+        public int? MaxNumberOfGuests { get; set; }
+
+        public bool PublishAvailability { get; set; }
+
+        public long StoreId { get; set; }
+
+    }
+}
