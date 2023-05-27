5fe31fe46b3c15c899a9b7a3a3ed40f26d47a16a
sraban75 <sraban75@outlook.com>
2023-05-27 19:51:59 +0600

    Merged

---
+++ aspnet-core/src/SoftGrid.EntityFrameworkCore/Migrations/20230521142937_CompletionPercentageAddedForTask.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftGrid.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompletionPercentageaddedforTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "TaskEvents",
+                newName: "TaskOrEvent");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CompletionPercentage",
+                table: "TaskEvents",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletionPercentage",
+                table: "TaskEvents");
+
+            migrationBuilder.RenameColumn(
+                name: "TaskOrEvent",
+                table: "TaskEvents",
+                newName: "Status");
+        }
+    }
+}
