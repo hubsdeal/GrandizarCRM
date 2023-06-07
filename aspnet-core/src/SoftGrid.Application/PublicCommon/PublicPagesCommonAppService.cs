b1c11a6a8366e0b42efead0257abab4f0a37f369
sraban75 <sraban75@outlook.com>
2023-06-07 20:17:14 +0600

    Updated

--- aspnet-core/src/SoftGrid.Application/PublicCommon/PublicPagesCommonAppService.cs
+++ aspnet-core/src/SoftGrid.Application/PublicCommon/PublicPagesCommonAppService.cs
@@ -115,7 +115,7 @@ namespace SoftGrid.PublicCommon
         //private readonly IRepository<ReservationTimeSlot, long> _reservationTimeSlotRepository;
         //private readonly IRepository<Reservation,long> _reservationRepository;
 
-        private readonly IRepository<HubNavigationMenu, long> _hubNavigationRepository;
+
         public PublicPagesCommonAppService(
             IRepository<ProductCategory, long> productCategoryRepository,
             IRepository<MediaLibrary, long> lookup_mediaLibraryRepository,
@@ -188,7 +188,7 @@ namespace SoftGrid.PublicCommon
             //IRepository<DeliveryType, int> deliveryTypeRepository,
             //IRepository<StoreDeliveryTypeMap, long> storeDeliveryTypeMapRepository,
             IRepository<StoreZipCodeMap, long> storeZipCodeMapRepository,
-            IRepository<StoreLocation, long> storeLocationRepository, IRepository<HubNavigationMenu, long> hubNavigationRepository
+            IRepository<StoreLocation, long> storeLocationRepository
             //IRepository<ReservationTimeSlot, long> reservationTimeSlotRepository,
             //IRepository<Reservation, long> reservationRepository
             )
@@ -267,7 +267,6 @@ namespace SoftGrid.PublicCommon
             //_storeDeliveryTypeMapRepository = storeDeliveryTypeMapRepository;
             //_storeZipCodeMapRepository = storeZipCodeMapRepository;
             _storeLocationRepository = storeLocationRepository;
-            _hubNavigationRepository = hubNavigationRepository;
             //_reservationTimeSlotRepository = reservationTimeSlotRepository;
             //_reservationRepository = reservationRepository;
         }
