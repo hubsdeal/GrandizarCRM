96b86c64b2268390e250a2a09638ef9d01402178
Rashedur Rahman <rashedronybd4u@gmail.com>
2023-07-14 19:37:24 +0600

    propduct category updated

--- angular/src/app/main/shop/productCategories/create-or-edit-productCategory-modal.component.ts
+++ angular/src/app/main/shop/productCategories/create-or-edit-productCategory-modal.component.ts
@@ -1,12 +1,15 @@
 ﻿import { Component, ViewChild, Injector, Output, EventEmitter, OnInit, ElementRef } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { finalize } from 'rxjs/operators';
-import { ProductCategoriesServiceProxy, CreateOrEditProductCategoryDto } from '@shared/service-proxies/service-proxies';
+import { ProductCategoriesServiceProxy, CreateOrEditProductCategoryDto, MediaLibrariesServiceProxy, CreateOrEditMediaLibraryDto } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { DateTime } from 'luxon';
 
 import { DateTimeService } from '@app/shared/common/timing/date-time.service';
 import { ProductCategoryMediaLibraryLookupTableModalComponent } from './productCategory-mediaLibrary-lookup-table-modal.component';
+import { FileItem, FileUploader, FileUploaderOptions } from 'ng2-file-upload';
+import { IAjaxResponse, TokenService } from 'abp-ng2-module';
+import { AppConsts } from '@shared/AppConsts';
 
 @Component({
     selector: 'createOrEditProductCategoryModal',
@@ -23,13 +26,22 @@ export class CreateOrEditProductCategoryModalComponent extends AppComponentBase 
     saving = false;
 
     productCategory: CreateOrEditProductCategoryDto = new CreateOrEditProductCategoryDto();
+    mediaLibrary: CreateOrEditMediaLibraryDto = new CreateOrEditMediaLibraryDto();
 
     mediaLibraryName = '';
+    allParentCategories: any[];
+    imageSrc: any = 'assets/common/images/c_logo.png';
+
+    private _uploaderOptions: FileUploaderOptions = {};
+    public uploader: FileUploader;
+    public temporaryPictureUrl: string;
 
     constructor(
         injector: Injector,
         private _productCategoriesServiceProxy: ProductCategoriesServiceProxy,
-        private _dateTimeService: DateTimeService
+        private _dateTimeService: DateTimeService,
+        private _tokenService: TokenService,
+        private _mediaLibrariesServiceProxy: MediaLibrariesServiceProxy
     ) {
         super(injector);
     }
@@ -39,6 +51,8 @@ export class CreateOrEditProductCategoryModalComponent extends AppComponentBase 
             this.productCategory = new CreateOrEditProductCategoryDto();
             this.productCategory.id = productCategoryId;
             this.mediaLibraryName = '';
+            this.temporaryPictureUrl = '';
+           
 
             this.active = true;
             this.modal.show();
@@ -52,23 +66,116 @@ export class CreateOrEditProductCategoryModalComponent extends AppComponentBase 
                 this.modal.show();
             });
         }
+        this._productCategoriesServiceProxy.getAllProductCategoryForTableDropdown().subscribe(result => {
+            this.allParentCategories = result;
+        });
+        this.initFileUploader();
     }
 
-    save(): void {
-        this.saving = true;
+    // save(): void {
+    //     this.saving = true;
 
-        this._productCategoriesServiceProxy
-            .createOrEdit(this.productCategory)
-            .pipe(
-                finalize(() => {
-                    this.saving = false;
-                })
-            )
-            .subscribe(() => {
-                this.notify.info(this.l('SavedSuccessfully'));
-                this.close();
-                this.modalSave.emit(null);
-            });
+    //     this._productCategoriesServiceProxy
+    //         .createOrEdit(this.productCategory)
+    //         .pipe(
+    //             finalize(() => {
+    //                 this.saving = false;
+    //             })
+    //         )
+    //         .subscribe(() => {
+    //             this.notify.info(this.l('SavedSuccessfully'));
+    //             this.close();
+    //             this.modalSave.emit(null);
+    //         });
+    // }
+    saveCategory(fileToken?: string): void {
+        if (fileToken != null) {
+            this.saving = true;
+            this.mediaLibrary.fileToken = fileToken;
+            this.mediaLibrary.masterTagCategoryId = 1;
+            this.mediaLibrary.masterTagId = 1;
+            this._mediaLibrariesServiceProxy.createOrEdit(this.mediaLibrary)
+                .pipe(finalize(() => { this.saving = false; }))
+                .subscribe((result) => {
+                    this.productCategory.mediaLibraryId = result;
+                    this._productCategoriesServiceProxy.createOrEdit(this.productCategory)
+                        .pipe(finalize(() => { this.saving = false; }))
+                        .subscribe(() => {
+                            this.notify.info(this.l('SavedSuccessfully'));
+                            this.close();
+                            this.modalSave.emit(null);
+                        });
+                });
+
+        } else {
+            this._productCategoriesServiceProxy.createOrEdit(this.productCategory)
+                .pipe(finalize(() => { this.saving = false; }))
+                .subscribe(() => {
+                    this.notify.info(this.l('SavedSuccessfully'));
+                    this.close();
+                    this.modalSave.emit(null);
+                });
+        }
+
+    }
+    save() {
+        if (this.uploader.queue != null && this.uploader.queue.length > 0) {
+            this.uploader.uploadAll();
+        } else {
+            this.saveCategory();
+        }
+    }
+
+
+
+    fileChangeEvent(event: any) {
+        this.mediaLibrary.name = event.target.files[0].name
+        this.mediaLibrary.size = event.target.files[0].size / 1024 + " kb";
+        this.mediaLibrary.fileExtension = event.target.files[0].type;
+        if (event.target.files && event.target.files[0]) {
+            var reader = new FileReader();
+
+            reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+            reader.onload = (event) => { // called once readAsDataURL is completed
+
+                this.imageSrc = event.target.result;
+            }
+        }
+    }
+    initFileUploader(): void {
+
+        this.uploader = new FileUploader({ url: AppConsts.remoteServiceBaseUrl + '/api/MediaUpload/UploadPicture' });
+        this._uploaderOptions.autoUpload = false;
+        this._uploaderOptions.authToken = 'Bearer ' + this._tokenService.getToken();
+        this._uploaderOptions.removeAfterUpload = true;
+        this.uploader.onAfterAddingFile = (file) => {
+            file.withCredentials = false;
+        };
+
+        this.uploader.onBuildItemForm = (fileItem: FileItem, form: any) => {
+            form.append('FileToken', this.guid());
+        };
+
+        this.uploader.onSuccessItem = (item, response, status) => {
+            const resp = <IAjaxResponse>JSON.parse(response);
+            if (resp.success) {
+                this.saveCategory(resp.result.fileToken);
+            } else {
+                this.message.error(resp.error.message);
+            }
+        };
+
+        this.uploader.setOptions(this._uploaderOptions);
+    }
+
+    guid(): string {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 
     openSelectMediaLibraryModal() {
@@ -92,5 +199,5 @@ export class CreateOrEditProductCategoryModalComponent extends AppComponentBase 
         this.modal.hide();
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void { }
 }
