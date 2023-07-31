785a3265f8179a0e051c2d8606cf34ef0b3c9af3
tofael-admin <tofael@vertis.digital>
2023-08-01 00:41:44 +0600

    admin-dashboard

--- angular/src/app/admin/dashboard/host-dashboard.component.ts
+++ angular/src/app/admin/dashboard/host-dashboard.component.ts
@@ -1,15 +1,17 @@
-import { Component, Injector, ViewEncapsulation } from '@angular/core';
+import { Component, Injector, ViewChild, ViewEncapsulation } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { DashboardCustomizationConst } from '@app/shared/common/customizable-dashboard/DashboardCustomizationConsts';
-
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+// import { AccordionModule } from 'primeng/accordion';
 @Component({
     templateUrl: './host-dashboard.component.html',
     styleUrls: ['./host-dashboard.component.less'],
     encapsulation: ViewEncapsulation.None,
+    animations: [appModuleAnimation()],
 })
 export class HostDashboardComponent extends AppComponentBase {
     dashboardName = DashboardCustomizationConst.dashboardNames.defaultHostDashboard;
-
+    // @ViewChild('p-accordion', { static: true }) paginator: AccordionModule;
     constructor(injector: Injector) {
         super(injector);
     }
