dccd0862d3ea1e6344b4a83ca27e16919361ef5f
Rashedur Rahman <rashedronybd4u@gmail.com>
2023-08-14 23:48:56 +0600

    task work items

--- angular/src/app/main/taskManagement/taskWorkItems/taskWorkItems.component.ts
+++ angular/src/app/main/taskManagement/taskWorkItems/taskWorkItems.component.ts
@@ -19,6 +19,7 @@ import { DateTime } from 'luxon';
 import { DateTimeService } from '@app/shared/common/timing/date-time.service';
 
 @Component({
+    selector: 'app-taskWorkItems',
     templateUrl: './taskWorkItems.component.html',
     encapsulation: ViewEncapsulation.None,
     animations: [appModuleAnimation()],
