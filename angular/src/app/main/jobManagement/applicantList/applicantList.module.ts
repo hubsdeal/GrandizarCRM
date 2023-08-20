e27928597a9208ea263f71f377268e942e2114d5
Dipro <dipro.hin@gmail.com>
2023-08-20 23:17:06 +0600

    design

--- angular/src/app/main/jobManagement/applicantList/applicantList.module.ts
+++ angular/src/app/main/jobManagement/applicantList/applicantList.module.ts
@@ -5,15 +5,23 @@ import {
     ApplicantListComponent
 } from "@app/main/jobManagement/applicantList/applicationListComponent/applicantList.component";
 import {SubheaderModule} from "@app/shared/common/sub-header/subheader.module";
+import {
+    ApplicantDashboardComponent
+} from "@app/main/jobManagement/applicantList/applicantDashboardComponent/applicantDashboard.component";
+import {FileUploadModule} from "@node_modules/primeng/fileupload";
+import {AdminSharedModule} from "@app/admin/shared/admin-shared.module";
 
 @NgModule({
     declarations: [
-        ApplicantListComponent
+        ApplicantListComponent,
+        ApplicantDashboardComponent
     ],
     imports: [
         AppSharedModule,
         ApplicantListRoutingModule,
-        SubheaderModule
+        SubheaderModule,
+        AdminSharedModule,
     ],
 })
-export class ApplicantListModule {}
+export class ApplicantListModule {
+}
