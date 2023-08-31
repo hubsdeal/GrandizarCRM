2a425b69c5d97005f1123cfbefd213e542cf7b96
Rashedur Rahman <rashedronybd4u@gmail.com>
2023-08-31 21:06:51 +0600

    store and products updated

--- angular/src/app/main/shop/storeProductMaps/create-or-edit-storeProductMap-modal.component.ts
+++ angular/src/app/main/shop/storeProductMaps/create-or-edit-storeProductMap-modal.component.ts
@@ -1,13 +1,16 @@
 ﻿import { Component, ViewChild, Injector, Output, EventEmitter, OnInit, ElementRef } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { finalize } from 'rxjs/operators';
-import { StoreProductMapsServiceProxy, CreateOrEditStoreProductMapDto } from '@shared/service-proxies/service-proxies';
+import { StoreProductMapsServiceProxy, CreateOrEditStoreProductMapDto, ProductsServiceProxy } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { DateTime } from 'luxon';
 
 import { DateTimeService } from '@app/shared/common/timing/date-time.service';
 import { StoreProductMapStoreLookupTableModalComponent } from './storeProductMap-store-lookup-table-modal.component';
 import { StoreProductMapProductLookupTableModalComponent } from './storeProductMap-product-lookup-table-modal.component';
+import { CreateOrEditProductDto } from '@shared/service-proxies/service-proxies';
+import { SelectItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'createOrEditStoreProductMapModal',
@@ -30,9 +33,17 @@ export class CreateOrEditStoreProductMapModalComponent extends AppComponentBase 
     storeName = '';
     productName = '';
 
+    storeId: number;
+    product: CreateOrEditProductDto = new CreateOrEditProductDto();
+    productServiceOptions: SelectItem[];
+    productCategoryOptions: any = []
+    selectedProductCategory: any;
+
     constructor(
         injector: Injector,
         private _storeProductMapsServiceProxy: StoreProductMapsServiceProxy,
+        private _productsServiceProxy: ProductsServiceProxy,
+        private _router: Router,
         private _dateTimeService: DateTimeService
     ) {
         super(injector);
@@ -50,7 +61,7 @@ export class CreateOrEditStoreProductMapModalComponent extends AppComponentBase 
         } else {
             this._storeProductMapsServiceProxy.getStoreProductMapForEdit(storeProductMapId).subscribe((result) => {
                 this.storeProductMap = result.storeProductMap;
-
+                this.storeId = result.storeProductMap.storeId;
                 this.storeName = result.storeName;
                 this.productName = result.productName;
 
@@ -60,21 +71,37 @@ export class CreateOrEditStoreProductMapModalComponent extends AppComponentBase 
         }
     }
 
+    // save(): void {
+    //     this.saving = true;
+    //     this.storeProductMap.storeId = this.storeId
+    //     this._storeProductMapsServiceProxy
+    //         .createOrEdit(this.storeProductMap)
+    //         .pipe(
+    //             finalize(() => {
+    //                 this.saving = false;
+    //             })
+    //         )
+    //         .subscribe(() => {
+    //             this.notify.info(this.l('SavedSuccessfully'));
+    //             this.close();
+    //             this.modalSave.emit(null);
+    //         });
+    // }
     save(): void {
         this.saving = true;
-
-        this._storeProductMapsServiceProxy
-            .createOrEdit(this.storeProductMap)
-            .pipe(
-                finalize(() => {
-                    this.saving = false;
-                })
-            )
-            .subscribe(() => {
+        if (this.storeId) {
+            this.product.storeId = this.storeId;
+        }
+        this._productsServiceProxy.createOrEdit(this.product)
+            .pipe(finalize(() => { this.saving = false; }))
+            .subscribe(result => {
                 this.notify.info(this.l('SavedSuccessfully'));
                 this.close();
                 this.modalSave.emit(null);
+                this._router.navigate(['/app/main/shop/products/dashboard/', result]);
             });
+
+
     }
 
     openSelectStoreModal() {
@@ -104,6 +131,37 @@ export class CreateOrEditStoreProductMapModalComponent extends AppComponentBase 
     getNewProductId() {
         this.storeProductMap.productId = this.storeProductMapProductLookupTableModal.id;
         this.productName = this.storeProductMapProductLookupTableModal.displayName;
+        if (this.storeProductMapProductLookupTableModal.id) {
+            this._productsServiceProxy.getProductForEdit(this.storeProductMapProductLookupTableModal.id).subscribe((result) => {
+                this.product.name = result.product.name;
+                this.product.storeId = result.product.storeId;
+                this.product.productCategoryId = result.product.productCategoryId;
+                this.product.mediaLibraryId = result.product.mediaLibraryId;
+                this.product.measurementUnitId = result.product.measurementUnitId;
+                this.product.currencyId = result.product.currencyId;
+                this.product.ratingLikeId = result.product.ratingLikeId;
+                this.product.contactId = result.product.contactId;
+                this.product.regularPrice = result.product.regularPrice;
+                this.product.salePrice = result.product.salePrice;
+                this.product.unitPrice = result.product.unitPrice;
+                this.product.measurementAmount = result.product.measurementAmount;
+                this.product.isService = result.product.isService;
+                this.product.stockQuantity = result.product.stockQuantity;
+                this.product.sku = result.product.sku;
+                this.product.url = result.product.url;
+                this.product.seoTitle = result.product.seoTitle;
+                this.product.metaKeywords = result.product.metaKeywords;
+                this.product.metaDescription = result.product.metaDescription;
+                this.product.shortDescription = result.product.shortDescription;
+                this.product.description = result.product.description;
+                this.product.internalNotes = result.product.internalNotes;
+                this.product.isPackageProduct = result.product.isPackageProduct;
+                this.product.isWholeSaleProduct = result.product.isWholeSaleProduct;
+                this.product.isPublished = result.product.isPublished;
+                this.product.isTaxExempt = result.product.isTaxExempt;
+                this.product.callForPrice = result.product.callForPrice;
+            });
+        }
     }
 
     close(): void {
@@ -111,5 +169,5 @@ export class CreateOrEditStoreProductMapModalComponent extends AppComponentBase 
         this.modal.hide();
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void { }
 }
