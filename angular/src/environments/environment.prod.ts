1d14f3a5ca00379deb92dcaca2c36516ef8352e7
Raasforce-Jobaar <raasforce@outlook.com>
2023-11-04 14:54:47 -0400

    refactor: dashboard tiles

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     production: true,
     hmr: false,
     appConfig: 'appconfig.production.json',
-    openAIapiKey: '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',
+    openAIapiKey: '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',
     OpenAIOrgId: 'org-INGV4qMNKJuyobDpzAAAasl4'
 };
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -7,6 +7,6 @@ export const environment = {
     production: false,
     hmr: false,
     appConfig: 'appconfig.json',
-    openAIapiKey: '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',
+    openAIapiKey: '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',
     OpenAIOrgId: 'org-INGV4qMNKJuyobDpzAAAasl4'
 };
